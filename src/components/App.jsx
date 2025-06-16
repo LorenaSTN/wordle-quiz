18840e86b923b71fd7cca0811b3a98e028f1d0d5
LorenaSTN <lorenstn@hotmail.com>
2025-06-16 12:53:45 +0200

    Input component: form and functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,33 @@
 import "../scss/App.scss";
+import {useState} from 'react'
+import Input from "./Input"
 
 function App() {
+  const [wordInput, setWordInput] = useState("")
+  const [guesses, setGuesses] = useState([])
+
+  // funct. input change
+  const handleInput = (ev) => {
+    setWordInput(ev.target.value)
+  }
+ 
+  // funct. submit form 
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+
+    if (wordInput.length < 5 || wordInput.length > 5){
+      window.alert("Please provide a word of 5 letters")
+      return; 
+    }
+    const nextWord = [...guesses, wordInput]
+    setGuesses(nextWord)
+    setWordInput("")
+  }
+
   return (
-    <div>
-      <h1></h1>
-    </div>
+  <>
+  <Input wordInput={wordInput} handleInput={handleInput} handleSubmit={handleSubmit} />
+  </>
   );
 }
 
---
+++ src/components/Input.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types"
+
+function Input({ wordInput, handleInput, handleSubmit}) {
+  return (
+    <>
+    <form onSubmit={handleSubmit}>
+    <input type="text" onChange={handleInput} value={wordInput}/>
+    </form>
+    </>
+  )
+}
+
+export default Input
+
+
+Input.propTypes = {
+    wordInput: PropTypes.string.isRequired,
+    handleInput: PropTypes.func.isRequired,
+    handleSubmit: PropTypes.func.isRequired, 
+}
